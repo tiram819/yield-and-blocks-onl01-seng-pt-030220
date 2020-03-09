15f8cb28c5ce7765ee8a23383170c709a2e0b3e0
Tabassum Hassan <tiram819@gmail.com>
2020-03-09 04:33:02 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-def hello_t
-
+def hello_t(array)
+   i = 0
+ 
+  while i < array.length
+    i = i + 1
+  end
+  
+  array
+else
+  puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
+
 
